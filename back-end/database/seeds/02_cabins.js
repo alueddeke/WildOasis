9e7fe91a3018afb2ef4f0d703cc4845861b46015
alueddeke <a.lueddeke@hotmail.com>
2024-05-02 18:19:33 -0400

    added delete cabin functionality, image path upload now functional

--- back-end/database/seeds/02_cabins.js
+++ back-end/database/seeds/02_cabins.js
@@ -10,11 +10,27 @@ exports.seed = function (knex) {
       return knex("cabins").insert([
         {
           id: 1,
+          name: "001",
           maxCapacity: 6,
           regularPrice: 300,
           discount: 15,
           description: "Spacious cabin with mountain view",
+          image: "data/cabins/cabin-001.jpg",
+        },
+        {
+          id: 2,
+          name: "002",
+          maxCapacity: 6,
+          regularPrice: 300,
+          discount: 15,
+          description: "Spacious cabin with mountain view",
+          image: "data/cabins/cabin-001.jpg",
         },
       ]);
     });
 };
+
+/*
+/Users/antonilueddeke/Desktop/lenovo-repos/the-wild-oasis-new/the-wild-oasis-V2/client/data/cabins/cabin-001.jpg 
+
+correct absolute path*/
